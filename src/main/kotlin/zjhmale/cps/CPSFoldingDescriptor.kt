fbc24c57f6f2f5cc141e74a7f1b7e40203bfc2ac
zjhmale <zjhmale@gmail.com>
2016-03-22 09:35:38 +0800

    rewrite descriptor use kotlin

---
+++ src/main/kotlin/zjhmale/cps/CPSFoldingDescriptor.kt
@@ -0,0 +1,20 @@
+package zjhmale.cps
+
+import com.intellij.lang.ASTNode
+import com.intellij.lang.folding.FoldingDescriptor
+import com.intellij.openapi.editor.FoldingGroup
+import com.intellij.openapi.util.TextRange
+
+/**
+ * Created by zjh on 16/3/22.
+ */
+class CPSFoldingDescriptor(node: ASTNode,
+                           range: TextRange,
+                           group: FoldingGroup,
+                           val name: String,
+                           val notExpandable: Boolean) : FoldingDescriptor(node, range, group) {
+
+    override fun getPlaceholderText() = name
+
+    override fun isNonExpandable() = notExpandable
+}
